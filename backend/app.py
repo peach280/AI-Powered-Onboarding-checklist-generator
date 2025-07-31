f7ede7bddfa6c30b7f2d34e47a33c54e9e656254
Vaishnavi Bhandari <vaishnavibhandari.128@gmail.com>
2025-07-31 09:56:29 +0000

    Updated app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+import requests
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "HELLO"
+
+@app.route("/scrape")
+def scrape_website():
+    url = request.args.get('url')
+    if not url:
+        return jsonify({"error":"URL NOT FOUND"}),400
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.content,'html.parser')
+        text = soup.get_text(separator=' ',strip=True)
+        return jsonify({"scraped_text":text})
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error":"Failed to fetch"}),500
