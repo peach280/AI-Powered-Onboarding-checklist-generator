c5022e3a2e5989bbd7be062c56580a5a9e6920b1
Vaishnavi Bhandari <vaishnavibhandari.128@gmail.com>
2025-11-04 00:25:23 +0000

    Final
    Signed-off-by: Vaishnavi Bhandari <vaishnavibhandari.128@gmail.com>

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,8 @@ from bs4 import BeautifulSoup
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import google.generativeai as genai
-from dotenv import load_dotenv
 
-load_dotenv()
+
 
 app = Flask(__name__)
 CORS(app)
